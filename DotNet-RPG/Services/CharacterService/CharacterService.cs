5006cf21a90709f200eb1a3b5cc7d06986ca10df
Angeredspy <christo.graham1@gmail.com>
2022-12-22 16:20:36 -0500

    Implements interface within CharacterService

--- DotNet-RPG/Services/CharacterService/CharacterService.cs
+++ DotNet-RPG/Services/CharacterService/CharacterService.cs
@@ -5,8 +5,21 @@ using System.Threading.Tasks;
 
 namespace DotNet_RPG.Services.CharacterService
 {
-    public class CharacterService
+    public class CharacterService : ICharacterService
     {
-        
+        public List<Character> AddCharacter(Character newCharacter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Character> GetAllCharacters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Character GetCharacterById(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
