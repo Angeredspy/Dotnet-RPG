d94b5803c79701d066ff192ae707623b2bf50e4b
Angeredspy <christo.graham1@gmail.com>
2022-12-24 23:50:53 -0500

    Implements asynchronus methods within ICharacterService

--- DotNet-RPG/Services/CharacterService/CharacterService.cs
+++ DotNet-RPG/Services/CharacterService/CharacterService.cs
@@ -11,18 +11,18 @@ namespace DotNet_RPG.Services.CharacterService
         new Character(),
         new Character {Id = 1, Name = "Zenitsu"}
       }; 
-        public List<Character> AddCharacter(Character newCharacter)
+        public async Task<List<Character>> AddCharacter(Character newCharacter)
         {
             characters.Add(newCharacter);
             return characters;
         }
 
-        public List<Character> GetAllCharacters()
+        public async Task<List<Character>> GetAllCharacters()
         {
             return characters;
         }
 
-        public Character GetCharacterById(int id)
+        public async Task<Character> GetCharacterById(int id)
         {
             var character = characters.FirstOrDefault(c => c.Id == id);
             if (character is not null) 
