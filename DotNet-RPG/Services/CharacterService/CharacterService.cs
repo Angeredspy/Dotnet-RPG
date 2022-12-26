6f26c2bfab86d8fe22bca02073da5ae3b4d7da41
Angeredspy <christo.graham1@gmail.com>
2022-12-26 10:56:03 -0500

    instantiates automapper & resolves type mismatches

--- DotNet-RPG/Services/CharacterService/CharacterService.cs
+++ DotNet-RPG/Services/CharacterService/CharacterService.cs
@@ -11,18 +11,24 @@ namespace DotNet_RPG.Services.CharacterService
         new Character(),
         new Character {Id = 1, Name = "Zenitsu"}
       }; 
+
+      private readonly IMapper _mapper;
+      public CharacterService(IMapper mapper)
+      {
+        _mapper = mapper;
+      }
         public async Task<ServiceResponse<List<GetCharacterDTO>>> AddCharacter(AddCharacterDTO newCharacter)
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
-            characters.Add(newCharacter);
-            serviceResponse.Data = characters;
+            characters.Add(_mapper.Map<Character>(newCharacter));
+            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToList();
             return serviceResponse;
         }
 
         public async Task<ServiceResponse<List<GetCharacterDTO>>> GetAllCharacters()
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
-            serviceResponse.Data = characters;
+            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToList();
             return serviceResponse;
         }
 
@@ -30,7 +36,7 @@ namespace DotNet_RPG.Services.CharacterService
         {
             var serviceResponse = new ServiceResponse<GetCharacterDTO>();
             var character = characters.FirstOrDefault(c => c.Id == id);
-            serviceResponse.Data = character;
+            serviceResponse.Data = _mapper.Map<GetCharacterDTO>(character);
             return serviceResponse;
         }
     }
