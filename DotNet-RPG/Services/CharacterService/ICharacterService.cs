4ea53f5f86914cf010566171e0c7040d778d4d98
Angeredspy <christo.graham1@gmail.com>
2022-12-26 13:37:35 -0500

    Creates updateCharacterDTO & adds corresponding method to interface

--- DotNet-RPG/Services/CharacterService/ICharacterService.cs
+++ DotNet-RPG/Services/CharacterService/ICharacterService.cs
@@ -10,5 +10,6 @@ namespace DotNet_RPG.Services.CharacterService
         Task<ServiceResponse<List<GetCharacterDTO>>> GetAllCharacters();
         Task<ServiceResponse<GetCharacterDTO>> GetCharacterById(int id);
         Task<ServiceResponse<List<GetCharacterDTO>>> AddCharacter(AddCharacterDTO newCharacter);
+        Task<ServiceResponse<GetCharacterDTO>> UpdateCharacter(UpdateCharacterDTO newCharacter);
     }
 }
